62f5449e7e5120815fa1ca94747fc118510ab266
FelSiq <felipe.siqueira@usp.br>
2017-04-15 20:13:42 -0300

    Added a few adressing study both on MIPS and in C.

---
+++ testing-lab/padding-in-c/paddingtest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct {
+	char a[3];
+	short int b;
+	int c;
+} myBelovedStruct;
+
+int main(int argc, char const *argv[]){
+	myBelovedStruct mbs = {"ABC", 0x123, 0x456789AB};
+
+	for (unsigned char *beholder = (unsigned char *) &mbs; 
+		beholder < (unsigned char *) &mbs + sizeof(myBelovedStruct); 
+		printf("%x ", *beholder++))
+		if (!(((unsigned long int) beholder) % 4)) printf("\n");
+
+	return 0;
+}
